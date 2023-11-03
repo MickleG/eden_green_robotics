2f20cb1422c6300780e04ea0e282fd2da6d0a56b
MickleG <micklethenickel@gmail.com>
2023-11-03 02:35:45 -0400

    initial commit

---
+++ test2.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory(host='128.46.190.249')
+
+led = LED(26, pin_factory=factory)
+
+while True:
+	input("Press Enter to turn LED on")
+	led.on()
+	input("turn it off")
+	led.off()
