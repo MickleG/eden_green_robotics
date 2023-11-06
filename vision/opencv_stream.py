3edbedd61bd6dc3ecfc8449623f8c793f237aa40
MickleG <micklethenickel@gmail.com>
2023-11-06 14:03:43 -0500

    added blue pixel masking to realsense rgb image

--- vision/opencv_stream.py
+++ vision/opencv_stream.py
@@ -1,5 +1,3 @@
-## Connecting Intel Realsense with Python and OpenCV
-
 import pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -10,9 +8,9 @@ pipe = rs.pipeline()
 cfg = rs.config()
 
 ## set up rgb streaming (size: 640x480, format: 8 bit, fps: 30)
-cfg.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+cfg.enable_stream(rs.stream.color, 424, 240, rs.format.bgr8, 30)
 ## set up depth streaming (16-bit format)
-cfg.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+cfg.enable_stream(rs.stream.depth, 424, 240, rs.format.z16, 30)
 
 ## start streaming
 pipe.start(cfg)
@@ -27,17 +25,18 @@ while True:
 	## convert images to numpy arrays
 	color_image = np.asanyarray(color_frame.get_data())
 	depth_image = np.asanyarray(depth_frame.get_data())
-	
-	depth_cm = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.5), cv2.COLORMAP_JET)
 
-	## show images
-	cv2.imshow('rgb', color_image)
-#	cv2.imshow('depth', depth_image)
-	cv2.imshow('depth', depth_cm)
+	# print(color_image)
+
+	mask = cv2.inRange(color_image, np.array([30, 0, 0]), np.array([255, 50, 30]))
+
+	cv2.imshow('mask', mask)
+
+	# depth_image_filtered = depth_image * mask
 
 	## Finds minimum z distance in frame
-	min_z = np.min(depth_image[np.nonzero(depth_image)])
-#	print(min_z)
+	min_z = np.min(depth_image[np.nonzero(depth_image)]) / 10000
+	print(min_z)
 
 	## break out of screen if press q
 	if cv2.waitKey(1) == ord('q'):
