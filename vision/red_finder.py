c827a2ee9e089ac8f7f0f798108478d72685bf86
ekrizma <ekrizma@purdue.edu>
2023-11-06 17:21:36 -0500

    added red_finder.py

--- vision/red_finder.py
+++ vision/red_finder.py
@@ -1,10 +1,10 @@
-
 import cv2
 import math
+import numpy as np
 
 threshold = 50
 
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(2)
 
 #insert code to calibrate start and end positions
 
@@ -14,19 +14,19 @@ def distance(pt1, pt2):
 while(vid.isOpened()):
 	ret, frame = vid.read()
 	if(ret):
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		# hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 		lower_red = np.array([0, 100, 100])
 		upper_red = np.array([10, 255, 255])
 
-		mask = cv2.inRange(hsv, lower_red, upper_red)
+		mask = cv2.inRange(frame, lower_red, upper_red)
 
-		color_image = cv2.bitwise_and(vid, vid, mask=mask)
+		# color_image = cv2.bitwise_and(vid, vid, mask=mask)
 
-		cv2.imshow('Red location', frame)
+		cv2.imshow('color', frame)
+		cv2.imshow('Red location', mask)
 
 	cv2.waitKey(5)
 
 vid.release()
 cv2.destroyAllWindows()
-
