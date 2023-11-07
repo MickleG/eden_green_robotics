b704cd08828d3706c9692b32410449ae0a98b7cb
MickleG <micklethenickel@gmail.com>
2023-11-07 16:46:54 -0500

    added remote stepper motor capability

--- autonomy/integrated_system/send_gpio_commands.py
+++ autonomy/integrated_system/send_gpio_commands.py
@@ -1,14 +1,25 @@
 from gpiozero import LED
+from gpiozero import PWMOutputDevice
+from gpiozero import OutputDevice
+from gpiozero import InputDevice
 from gpiozero.pins.pigpio import PiGPIOFactory
+import time
 
 factory = PiGPIOFactory(host='128.46.190.249')
 
-led = LED(26, pin_factory=factory)
+# led = LED(19, pin_factory=factory)
+step = OutputDevice(pin=27, pin_factory=factory)
+direction = OutputDevice(4, pin_factory=factory)
 
-while True:
-	input("Press Enter to turn LED on")
-	led.on()
-	input("turn it off")
-	led.off()
 
-#elena was here
+for i in range(10000):
+	step.on()
+	time.sleep(0.001)
+	step.off()
+	time.sleep(0.0005)
+
+# while True:
+# 	input("Press Enter to turn LED on")
+# 	led.on()
+# 	input("turn it off")
+# 	led.off()
