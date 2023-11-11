ddcf453ac4c4a2c112b41538f50a5af1238e3be4
MickleG <micklethenickel@gmail.com>
2023-11-11 17:50:01 -0500

    updated server code

--- autonomy/positioning/server.py
+++ autonomy/positioning/server.py
@@ -3,15 +3,18 @@ import time
 from threading import Timer
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("128.46.190.198", 9000))
+s.bind(("", 9000))
 s.listen(5)
 print("server is now running")
 
 def background_controller():
-	message = "hello!"
+	message = str(time.time_ns())
+	print(message)
 	clientsocket.send(bytes(message, "utf-8"))
-	Timer(0.01, background_controller).start()
+
+clientsocket, address = s.accept()
+print(f"Connection from {address} has been established")
+
 while(True):
-	clientsocket, address = s.accept()
-	print(f"Connection from {address} has been established")
 	background_controller()
+	time.sleep(0.1)
