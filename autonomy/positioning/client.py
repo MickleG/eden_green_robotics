d8210916db4e98925123d8bca33aad46d7770c1d
MickleG <micklethenickel@gmail.com>
2023-11-11 17:26:38 -0500

    updated client code

--- autonomy/positioning/client.py
+++ autonomy/positioning/client.py
@@ -1,7 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("128.46.190.198", 9000))
+# s.connect(("128.46.190.198", 9000))
+s.connect(("10.211.55.3", 9000))
 
 while(True):
 	print(s.recv(1024).decode("utf-8"))
--- autonomy/positioning/server.py
+++ autonomy/positioning/server.py
@@ -3,14 +3,17 @@ import time
 from threading import Timer
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 9000))
+hostname = socket.gethostname()
+s.bind((hostname, 9000))
+print(hostname)
+print(socket.gethostbyname(hostname))
 s.listen(5)
 print("server is now running")
 
 def background_controller():
 	message = "hello!"
 	clientsocket.send(bytes(message, "utf-8"))
-	Timer(0.005, background_controller).start()
+	Timer(5, background_controller).start()
 while(True):
 	clientsocket, address = s.accept()
 	print(f"Connection from {address} has been established")
