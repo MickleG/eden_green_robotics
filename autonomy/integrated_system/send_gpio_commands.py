09b92a6bdd5b1cbb5391c25f429ef7157bf7e39d
MickleG <micklethenickel@gmail.com>
2023-11-13 16:34:10 -0500

    updated server code

--- autonomy/integrated_system/send_gpio_commands.py
+++ autonomy/integrated_system/send_gpio_commands.py
@@ -1,17 +1,18 @@
+#direction1.on() and direction2.off() goes to robot's right
 from gpiozero import OutputDevice
-from gpiozero.pins.pigpio import PiGPIOFactory
-
 import time
 
-factory = PiGPIOFactory(host='128.46.190.198')
+step1 = OutputDevice(pin=23)
+direction1 = OutputDevice(pin=27)
 
-step1 = OutputDevice(pin=23, pin_factory=factory)
-direction1 = OutputDevice(pin=27, pin_factory=factory)
-
-step2 = OutputDevice(pin=4, pin_factory=factory)
-direction2 = OutputDevice(pin=21, pin_factory=factory)
+step2 = OutputDevice(pin=4)
+direction2 = OutputDevice(pin=21)
 
 while(True):
-	step1.on()
-	step1.off()
-	time.sleep(0.000003)
+    step1.on()
+    step1.off()
+    step2.on()
+    step2.off()
+    direction1.on()
+    direction2.off()
+    time.sleep(0.000003)
--- autonomy/positioning/server.py
+++ autonomy/positioning/server.py
@@ -1,20 +1,51 @@
 import socket
 import time
-from threading import Timer
+from gpiozero import OutputDevice
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", 9000))
-s.listen(5)
+addr = "128.46.190.198"
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((addr, 9000))
+#s.listen(1)
 print("server is now running")
 
-def background_controller():
-	message = str(time.time_ns())
-	print(message)
-	clientsocket.send(bytes(message, "utf-8"))
-
-clientsocket, address = s.accept()
-print(f"Connection from {address} has been established")
+#clientsocket, address = s.accept()
+#print(f"Connection from {address} has been established")
+print(f"Connection from {addr} has been established")
+step1 = OutputDevice(pin=23)
+dir1 = OutputDevice(pin=27)
+step2 = OutputDevice(pin=4)
+dir2 = OutputDevice(pin=21)
+kp = 0.000003
+e = 0
 
 while(True):
-	background_controller()
-	time.sleep(0.1)
+    #message = "image_requested"
+    #clientsocket.send(bytes(message, "utf-8"))
+    #received_msg = clientsocket.recv(1024).decode("utf-8")
+    received_msg, address = s.recvfrom(1024)
+    received_msg = received_msg.decode("utf-8")
+    step1.on()
+    step2.on()
+    step1.off()
+    step2.off()
+    if(e == 0):
+        time.sleep(kp)
+    else:
+        time.sleep(abs(kp / e))
+
+    if(received_msg):
+        if(";" in received_msg):
+            pass
+        else:
+            e = int(received_msg)
+            #print("e is {}".format(e))
+
+            if(abs(e) > 20):
+                if(e > 0):
+                    #print("error positive")
+                    dir1.off()
+                    dir2.on()
+                elif(e < 0):
+                    #print("error negative")
+                    dir1.on()
+                    dir2.off()
