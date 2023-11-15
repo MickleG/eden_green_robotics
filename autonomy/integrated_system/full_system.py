13f1bfd219cb5281b0294d7137fd16f04096c4d0
MickleG <micklethenickel@gmail.com>
2023-11-14 19:05:06 -0500

    updated client code for alignment module

--- autonomy/integrated_system/full_system.py
+++ autonomy/integrated_system/full_system.py
@@ -1,4 +1,8 @@
 # coordinate system of RealSense (as mounted): X to the camera's right, Y to the camera's bottom, Z away from the camera
+# 80mm between back ee plate and horizontal tangent of sheath
+# 23mm thickness of realsense
+# distance between front of realsense and horizontal tangent of sheath = 57mm
+# target e_z = 65mm?
 
 import pyrealsense2 as rs
 import numpy as np
@@ -12,6 +16,12 @@ sys.path.append("../../vision")
 from vision_main import depth_stream
 from vision_main import collect_vine_mask
 
+import socket
+
+# setting up comms between pi and computer
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s.connect(("128.46.190.198", 9000)) # replace IP address with Raspberry Pi IP address
+addr = ("128.46.190.198", 9000)
 median_filter_buffer_u = []
 median_filter_buffer_v = []
 
@@ -19,8 +29,10 @@ median_filter_size = 1
 record_min_z = False
 avg_min_z_u = 0
 avg_min_z_v = 0
+TARGET_Z = 65
 
 resolution = [424, 240]
+finding_cup = False
 
 # make connection to webcam
 pipe = rs.pipeline()
@@ -28,9 +40,9 @@ pipe = rs.pipeline()
 cfg = rs.config()
 
 # set up rgb streaming (size: 424x240, format: 8 bit, fps: 30)
-cfg.enable_stream(rs.stream.color, resolution[0], resolution[1], rs.format.bgr8, 30)
+cfg.enable_stream(rs.stream.color, resolution[0], resolution[1], rs.format.bgr8, 15)
 # set up depth streaming (16-bit format)
-cfg.enable_stream(rs.stream.depth, resolution[0], resolution[1], rs.format.z16, 30)
+cfg.enable_stream(rs.stream.depth, resolution[0], resolution[1], rs.format.z16, 15)
 
 # start streaming
 pipe.start(cfg)
@@ -39,56 +51,44 @@ while(True):
 
 	depth_image, color_image, cup_mask, cup_u_indices, cup_v_indices = depth_stream(pipe)
 
-	if(len(cup_v_indices) > 900): # cup detected, code for cup operations goes here
+	if(len(cup_v_indices) > 900 and finding_cup): # cup detected, code for cup operations goes here
 		avg_cup_u = int(statistics.mean(cup_u_indices))
 		avg_cup_v = int(statistics.mean(cup_v_indices))
 		avg_min_z_u = None
 		avg_min_z_v = None
 
+		# s.send(bytes(str(avg_cup_u) + ";" + str(avg_cup_v), "utf-8"))
+		s.sendto(bytes(str(avg_cup_u) + ":" + str(avg_cup_v), "utf-8"), addr) #signifying cup command with ":" to limit size of message
+
+
 	else: # cup not detected, hugging vine code here
 		avg_cup_u = None
 		avg_cup_v = None
 
-		vine_mask = collect_vine_mask(color_image)
-
-		cv2.imshow("vine_mask", vine_mask)
-
-		filtered_depth_image = cv2.bitwise_and(depth_image, depth_image, mask=vine_mask)
-
-		min_val = np.min(filtered_depth_image[np.nonzero(filtered_depth_image)])
-		min_z_index = list(zip(*np.where(filtered_depth_image == min_val)))
+		min_val = np.min(depth_image[np.nonzero(depth_image)])
+		min_z_index = list(zip(*np.where(depth_image == min_val)))
 
 		min_z_v = min_z_index[0][0]
 		min_z_u = min_z_index[0][1]
-
 
 		avg_min_z_u = min_z_u
 		avg_min_z_v = int(resolution[1] / 2)
 
 
-		# ##median filtering the min_z indices to achieve less noise
-		# if(len(median_filter_buffer_u) <= median_filter_size):
-		# 	median_filter_buffer_u.append(min_z_u)
-		# 	# median_filter_buffer_v.append(min_z_v)
-		# else:
-		# 	avg_min_z_u = int(statistics.mean(median_filter_buffer_u))
-		# 	# avg_min_z_v = int(statistics.mean(median_filter_buffer_v))
-		# 	median_filter_buffer_u.pop(0)
-		# 	# median_filter_buffer_v.pop(0)
-		# 	median_filter_buffer_u.append(min_z_u)
-		# 	# median_filter_buffer_v.append(min_z_v)
+		# calculating error and sending result to raspberry pi to run motors
+		e_x = int(resolution[0] / 2 - avg_min_z_u)
+		e_z = min_val - TARGET_Z
+		s.sendto(bytes(str(e_x) + ";" + str(int(e_z / 10)), "utf-8"), addr) #signifying hug command with ";" to limit size of message
+		
 
-		e = int(resolution[0] / 2 - avg_min_z_u)
-		# print(e)
-
-	color_image = cv2.circle(color_image, (avg_cup_u, avg_cup_v), radius=2, color=[0, 255, 0], thickness=-1)
+	# color_image = cv2.circle(color_image, (avg_cup_u, avg_cup_v), radius=2, color=[0, 255, 0], thickness=-1)
 	color_image = cv2.circle(color_image, (avg_min_z_u, avg_min_z_v), radius=2, color=[0, 0, 255], thickness=-1)
 
-	result = cv2.bitwise_and(depth_image, depth_image, mask=cup_mask)
+	# result = cv2.bitwise_and(depth_image, depth_image, mask=cup_mask)
 
 
 	# cv2.imshow('Masked image', result)
-	cv2.imshow('mask', cup_mask)
+	# cv2.imshow('mask', cup_mask)
 	cv2.imshow('marked centroid', color_image)
 	## break out of screen if press q
 	if cv2.waitKey(1) == ord('q'):
--- autonomy/positioning/client.py
+++ autonomy/positioning/client.py
@@ -1,8 +1,9 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect(("128.46.190.198", 9000))
-s.connect(("10.211.55.3", 9000))
+s.connect(("128.46.190.198", 9002))
 
 while(True):
-	print(s.recv(1024).decode("utf-8"))
+	received_message = s.recv(1024).decode("utf-8")
+	if(received_message == "image_requested"):
+		s.send(bytes("ack", "utf-8"))
