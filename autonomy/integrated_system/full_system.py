f57a0eef5f542a0525baaef1adb285308e2bfb9d
MickleG <micklethenickel@gmail.com>
2023-12-07 01:49:54 -0500

    added blue detected as a message, simplified code

--- autonomy/integrated_system/full_system.py
+++ autonomy/integrated_system/full_system.py
@@ -27,26 +27,21 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #s.connect(("128.46.190.198", 9000)) # replace IP address with Raspberry Pi IP address
 addr = ("128.46.190.170", 9000)
-median_filter_buffer_u = []
-median_filter_buffer_v = []
 
-median_filter_size = 1
-record_min_z = False
 avg_min_z_u = None
 avg_min_z_v = None
+blue_detected = False
 TARGET_Z = 75
 
 cup_offset = 50 # value used to determine where to filter z
 
 resolution = [424, 240]
-finding_cup = False
 
 # make connection to webcam
 pipe = rs.pipeline()
 # make variable for initiation calls
 cfg = rs.config()
 
-min_z_u_storage = []
 
 # set up rgb streaming (size: 424x240, format: 8 bit, fps: 30)
 cfg.enable_stream(rs.stream.color, resolution[0], resolution[1], rs.format.bgr8, 30)
@@ -65,44 +60,30 @@ while(True):
 
 	new_resolution = depth_image.shape
 
-	# if(len(cup_v_indices) > 900 and finding_cup): # cup detected, code for cup operations goes here
-		# avg_cup_u = int(statistics.mean(cup_u_indices))
-		# avg_cup_v = int(statistics.mean(cup_v_indices))
-		# avg_min_z_u = None
-		# avg_min_z_v = None
+	if(len(cup_v_indices) > 900): # cup detected, code for cup operations goes here
+		blue_detected = True
 
-		# # s.send(bytes(str(avg_cup_u) + ";" + str(avg_cup_v), "utf-8"))
-		# s.sendto(bytes(str(avg_cup_u) + ":" + str(avg_cup_v), "utf-8"), addr) #signifying cup command with ":" to limit size of message
+	else:
+		blue_detected = False
 
+	avg_cup_u = None
+	avg_cup_v = None
 
-	if(True): # cup not detected, hugging vine code here
-		avg_cup_u = None
-		avg_cup_v = None
-
-		a = np.array(depth_image)
-
-		min_val = np.min(depth_image[np.nonzero(depth_image)]) + cup_offset * 10 # this value shows the cutoff for points to take the x-centroid at
-
-		filtered_depth_image = np.where(np.array(depth_image) < min_val, np.array(depth_image), 0)
-		avg_min_z_u = int(np.array(list(map(list, zip(*np.nonzero(filtered_depth_image))))).mean(axis=0)[1])
-		avg_min_z_v = int(new_resolution[0] / 2)
+	min_val = np.min(depth_image[np.nonzero(depth_image)]) + cup_offset * 10 # this value shows the cutoff for points to take the x-centroid at
+	filtered_depth_image = np.where(np.array(depth_image) < min_val, np.array(depth_image), 0)
+	avg_min_z_u = int(np.array(list(map(list, zip(*np.nonzero(filtered_depth_image))))).mean(axis=0)[1])
+	avg_min_z_v = int(new_resolution[0] / 2)
 
 
-		# calculating error and sending result to raspberry pi to run motors
-		e_x = int(new_resolution[1] / 2 - avg_min_z_u)
-		e_z = min_val
-		# print(e_z)
-		s.sendto(bytes(str(e_x) + ";" + str(int(e_z / 10)), "utf-8"), addr) #signifying hug command with ";" to limit size of message
+	# calculating error and sending result to raspberry pi to run motors
+	e_x = int(new_resolution[1] / 2 - avg_min_z_u)
+	e_z = min_val
+	# print(e_z)
+	s.sendto(bytes(str(e_x) + ";" + str(int(e_z / 10)) + ";" + str(blue_detected), "utf-8"), addr) #signifying hug command with ";" to limit size of message
 
 	color_image = cv2.circle(color_image, (avg_cup_u, avg_cup_v), radius=2, color=[0, 255, 0], thickness=-1)
 	color_image = cv2.circle(color_image, (avg_min_z_u, avg_min_z_v), radius=2, color=[0, 0, 255], thickness=-1)
 
-
-	# result = cv2.bitwise_and(depth_image, depth_image, mask=cup_mask)
-
-
-	# cv2.imshow('Masked image', result)
-	# cv2.imshow('mask', cup_mask)
 	cv2.imshow('marked centroid', color_image)
 	## break out of screen if press q
 	if cv2.waitKey(1) == ord('q'):
