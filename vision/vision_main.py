251bc2ecab4ccabf1b2e9395e7994e56fb1ed93a
MickleG <micklethenickel@gmail.com>
2023-12-13 17:06:51 -0500

    improved x-centroid vision code

--- vision/vision_main.py
+++ vision/vision_main.py
@@ -3,6 +3,11 @@ import numpy as np
 import cv2
 import statistics
 import time
+import math
+
+
+def distance(pt1, pt2):
+	return(math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2))
 
 def depth_stream(pipe):
 
@@ -19,7 +24,7 @@ def depth_stream(pipe):
 	height = dimensions[0]
 	width = dimensions[1]
 
-	mask = cv2.inRange(color_image, np.array([30, 0, 0]), np.array([255, 50, 30]))
+	mask = cv2.inRange(color_image, np.array([50, 0, 0]), np.array([255, 50, 30]))
 
 	isolated_mask = np.nonzero(mask)
 
@@ -32,111 +37,52 @@ def depth_stream(pipe):
 
 	return(depth_image, color_image, mask, u_indices, v_indices)
 
-def collect_vine_mask(color_image):
-	vine_mask = cv2.inRange(color_image, np.array([0, 0, 0]), np.array([60, 60, 60]))
+def fiducials(frame):
+	brighter = cv2.convertScaleAbs(frame, alpha = 3, beta = 10)
+	hsv = frame
 
-	isolated_mask = np.nonzero(vine_mask)
+	pt1 = (0,0)
+	pt2 = (0,0)
 
-	kernel = np.ones((4, 4), np.uint8)
-	vine_mask = cv2.erode(vine_mask, kernel, iterations=1)
-	vine_mask = cv2.dilate(vine_mask, kernel, iterations=1)
+	height, width, _ = frame.shape
+	roi_start = int(0.5*width)
+	roi = hsv[:, roi_start:]
 
-	return(vine_mask)
+	lower_red = np.array([0, 0, 100])
+	upper_red = np.array([70, 70, 255])
 
-def distance(pt1, pt2):
-	return(math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2))
+	mask = cv2.inRange(roi, lower_red, upper_red)
 
-def scissor_distance()
-	vid = cv2.VideoCapture(2, cv2.CAP_DSHOW)
+	v_indices_r,u_indices_r = np.nonzero(mask)
 
-	while(vid.isOpened()):
-		ret, frame = vid.read()
-		if(ret):
-			brighter = cv2.convertScaleAbs(frame, alpha = 3, beta = 25)
-			hsv = cv2.cvtColor(brighter, cv2.COLOR_BGR2HSV)
-			# (h, s, v) = cv2.split(hsv)
+	if u_indices_r.size > 0:
 
-			# s = s * 1
-			# s = np.clip(s,0,255)
-
-			# hsv = cv2.merge([h,s,v])
-
-			pt1 = (0,0)
-			pt2 = (0,0)
-
-			height, width, _ = frame.shape
-			roi_start = int(0.5*width)
-			top = int(height * 4/ 9)
-			bottom = int(height * 5 / 9)
-			roi = hsv[top:bottom, roi_start:]
-
-			lower_red = np.array([0, 60, 60])
-			upper_red = np.array([10, 255, 255])
-
-			mask = cv2.inRange(roi, lower_red, upper_red)
-
-			kernel = np.ones((4,4), np.uint8)
-			mask = cv2.erode(mask, kernel, iterations=1)
-			mask = cv2.dilate(mask, kernel, iterations=1)
-
-			v_indices_r,u_indices_r = np.nonzero(mask)
-
-			if u_indices_r.size > 0:
-
-				avg_ur = int(statistics.mean(u_indices_r)+ (0.5*width)) 
-				avg_vr = int(statistics.mean(v_indices_r))
-
-				red_ptr = cv2.circle(brighter, (avg_ur, avg_vr), radius=4, color = [0,0,255], thickness=-2)
-				pt1 = (avg_ur, avg_vr)
+		avg_ur = int(statistics.mean(u_indices_r)+ (0.5*width)) 
+		avg_vr = int(statistics.mean(v_indices_r))
+		pt1 = (avg_ur, avg_vr)
 
 
-			#left
-			roi_l = hsv[top:bottom, int(width/10):roi_start,:]
+	#left
+	roi_l = hsv[:,:roi_start]
 
-			mask_l = cv2.inRange(roi_l, lower_red, upper_red)
+	mask_l = cv2.inRange(roi_l, lower_red, upper_red)
 
-			mask_l = cv2.erode(mask_l, kernel, iterations=1)
-			mask_l = cv2.dilate(mask_l, kernel, iterations=1)
+	v_indices_l,u_indices_l = np.nonzero(mask_l)
 
-			v_indices_l,u_indices_l = np.nonzero(mask_l)
+	if u_indices_l.size > 0:
 
-			if u_indices_l.size > 0:
+		avg_ul = int(statistics.mean(u_indices_l)+ int(width/10)) 
+		avg_vl = int(statistics.mean(v_indices_l))
 
-				avg_ul = int(statistics.mean(u_indices_l)+ int(width/10)) 
-				avg_vl = int(statistics.mean(v_indices_l))
+		pt2 = (avg_ul, avg_vl)
 
-				red_ptl = cv2.circle(brighter, (avg_ul, avg_vl), radius=4, color = [0,0,255], thickness=-2)
+	red_dist_avg = int(distance(pt1, pt2))
 
-				pt2 = (avg_ul, avg_vl)
+	if(red_dist_avg < 250):
+		cut_complete = 1
+	else:
+		cut_complete = 0
+		
+	return(cut_complete)
 
-
-			red_dist = (distance(pt1, pt2))
-
-			last10 = []
-
-			i = 0
-			if i < 100:
-				last10.append(red_dist)
-				i +=1
-			else:
-				last10.remove(0)
-				last10.append(red_dist)
-
-			red_dist_avg = statistics.median(last10)
-
-			centeru = int(width * 0.5)
-			centerv = int(height * 0.5)
-
-		cv2.waitKey(5)
-
-	vid.release()
-	cv2.destroyAllWindows()
-	red_dist_avg = int(red_dist_avg)
-
-	return(red_dist_avg)
-
-
-
-
-
-
+	cv2.imshow("color", hsv)
